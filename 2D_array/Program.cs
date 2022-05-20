701787eec4efe1727a490b94f3d4412e337db829
reyes221093 <102719907+reyes221093@users.noreply.github.com>
2022-05-20 14:05:57 +0800

    Add project files.

---
+++ 2D_array/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+String[,] array_2D = {
+    {"a","b","c"},
+    {"d","e","f"},
+};
+Console.WriteLine(array_2D[0,0]);
+Console.WriteLine(array_2D[0,1]);
+Console.WriteLine(array_2D[0, 2]);
+Console.WriteLine(array_2D[1, 0]);
+Console.WriteLine(array_2D[1, 1]);
+Console.WriteLine(array_2D[1, 2]);
